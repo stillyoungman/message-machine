a5e57cd92dd6e6bd9db1044bb070d64b4cfdc75c
Constantine Nikolsky <myheavybox@gmail.com>
2018-12-07 03:48:01 +0300

    implement notification sound

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,16 @@
 'use strict'
 
 function handleSend() {
-    toastr["success"]("message", "title")
+    toastr["success"]("message", "title");
 }
 
 function handleNewConnection() {
     toastr["info"]("message", "title")
 }
+
+function playSound(filename){
+    var mp3Source = '<source src="assets/' + filename + '.mp3" type="audio/mpeg">';
+    var oggSource = '<source src="assets/' + filename + '.ogg" type="audio/ogg">';
+    var embedSource = '<embed hidden="true" autostart="true" loop="false" src="' + filename +'.mp3">';
+    document.getElementById("sound").innerHTML='<audio autoplay="autoplay">' + mp3Source + oggSource + embedSource + '</audio>';
+  }
