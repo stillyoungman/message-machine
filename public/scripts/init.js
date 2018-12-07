e99314fa32d8a1d412acc2fbdf66cdc641c32474
Constantine Nikolsky <myheavybox@gmail.com>
2018-12-07 03:41:05 +0300

    refactoring

---
+++ public/scripts/init.js
@@ -0,0 +1,21 @@
+document.getElementById("send").addEventListener("click", function (event) {
+    event.preventDefault()
+});
+
+toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "700",
+    "timeOut": "2000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
